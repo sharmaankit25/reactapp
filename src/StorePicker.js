f3d1892825d371ebfc3a34dc94687b88c5baf2df
Ankit Sharma <sandyhiet@gmail.com>
2017-03-15 12:33:58 +0530

    Create a global state in storepicker

--- src/StorePicker.js
+++ src/StorePicker.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 
 class StorePicker extends Component {
+  constructor(){
+    super();
+    this.state = {name:"Ankit"}
+  }
+
+
   goToStore(e){
     e.preventDefault();
     var storeId = this.refs.storeId.value;
+    this.setState({name:storeId});
+
     
   }
   render(){
     return(
-
+      <div>
+      <h2>{this.state.name}</h2>
       <form onSubmit={this.goToStore.bind(this)}>
         <input type="text" ref="storeId" placeholder="Enter name" />
         <button type="submit" >Click here!!</button>
       </form>
-
+      </div>
     );
   }
 
